ddb73067e6e087fc1d9bf002d14fa48e5f969a2a
JiWon Kim <lost731@naver.com>
2022-01-27 18:24:37 +0900

    Feat: url splicing to recongnize problem is for contest or assignment

--- projects/main/src/app/pages/problem-pages/problem-form-page/problem-form-page.component.ts
+++ projects/main/src/app/pages/problem-pages/problem-form-page/problem-form-page.component.ts
@@ -15,40 +15,39 @@ import { LayoutService } from '../../../services/layout.service';
 @Component({
   selector: 'sw-problem-form-page',
   templateUrl: './problem-form-page.component.html',
-  styleUrls: ['./problem-form-page.component.scss']
+  styleUrls: ['./problem-form-page.component.scss'],
 })
 export class ProblemFormPageComponent extends AbstractFormDirective<IProblem, string> implements OnInit {
-
   contest: IContest;
   errorMatcher = new ErrorMatcher(this.submitted$, this.submissionError$);
 
-  constructor(public auth: AuthService,
-              public layout: LayoutService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private contestService: ContestService,
-              private problemService: ProblemService,
-              fb: FormBuilder) {
+  constructor(
+    public auth: AuthService,
+    public layout: LayoutService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private contestService: ContestService,
+    private problemService: ProblemService,
+    fb: FormBuilder
+  ) {
     super(fb);
   }
 
   cancel(): void {
     if (this.modifying) {
-      this.contest ?
-        this.router.navigate(['/problem/detail', this.model._id], { queryParams: { contest: this.contest._id } }) :
-        this.router.navigate(['/problem/detail', this.model._id]);
+      this.contest
+        ? this.router.navigate(['/problem/detail', this.model._id], { queryParams: { contest: this.contest._id } })
+        : this.router.navigate(['/problem/detail', this.model._id]);
     } else {
-      this.contest ?
-        this.router.navigate(['/contest', this.contest._id, 'problems']) :
-        this.router.navigateByUrl('/problem/list/me');
+      this.contest
+        ? this.router.navigate(['/contest', this.contest._id, 'problems'])
+        : this.router.navigateByUrl('/problem/list/me');
     }
   }
-
 
   async submit(): Promise<void> {
     await super.submit();
   }
-
 
   protected async processAfterSubmission(s: string): Promise<void> {
     if (this.contest) {
@@ -81,18 +80,21 @@ export class ProblemFormPageComponent extends AbstractFormDirective<IProblem, st
     });
   }
 
-
   protected submitObservable(m: IProblem): Observable<string> {
     let observable: Observable<string>;
-
+    let assignmentId: string = new URL(window.location.href).searchParams.get('assignment');
+    let contestId: string = new URL(window.location.href).searchParams.get('contest');
+    console.log(assignmentId, contestId);
     if (this.contest) {
-      observable = this.modifying ?
-        this.contestService.updateContestProblem(this.contest._id, this.model._id, m).pipe(map(()=> this.contest._id)) :
-        this.contestService.createContestProblem(this.contest._id, m).pipe(map(() => this.contest._id));
+      observable = this.modifying
+        ? this.contestService
+            .updateContestProblem(this.contest._id, this.model._id, m)
+            .pipe(map(() => this.contest._id))
+        : this.contestService.createContestProblem(this.contest._id, m).pipe(map(() => this.contest._id));
     } else {
-      observable = this.modifying ?
-        this.problemService.updateProblem(this.model._id, m).pipe(map(() => this.model._id)) :
-        this.problemService.createProblem(m).pipe(map(res => res.data._id));
+      observable = this.modifying
+        ? this.problemService.updateProblem(this.model._id, m).pipe(map(() => this.model._id))
+        : this.problemService.createProblem(m).pipe(map((res) => res.data._id));
     }
 
     return observable;
@@ -102,20 +104,21 @@ export class ProblemFormPageComponent extends AbstractFormDirective<IProblem, st
     super.ngOnInit();
 
     this.addSubcription(
-      this.route.params.pipe(
-        map(params => params.id),
-        filter(id => !!id),
-        switchMap(id => this.problemService.getProblem(id))
-      ).subscribe(
-        res => this.model = res.data
-      ),
+      this.route.params
+        .pipe(
+          map((params) => params.id),
+          filter((id) => !!id),
+          switchMap((id) => this.problemService.getProblem(id))
+        )
+        .subscribe((res) => (this.model = res.data)),
 
-      this.route.queryParams.pipe(
-        map(params => params.contest),
-        filter(id => !!id),
-        switchMap(id => this.contestService.getContest(id))
-      ).subscribe(
-        res => this.contest = res.data
-    ));
+      this.route.queryParams
+        .pipe(
+          map((params) => params.contest),
+          filter((id) => !!id),
+          switchMap((id) => this.contestService.getContest(id))
+        )
+        .subscribe((res) => (this.contest = res.data))
+    );
   }
 }
