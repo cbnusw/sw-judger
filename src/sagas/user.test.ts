46f03faff62a8b9f825c65e7542473a53edaedc8
Jiwon Kim <lost731@naver.com>
2022-02-05 15:12:51 +0900

    Fix: saga test code

--- src/sagas/user.test.ts
+++ src/sagas/user.test.ts
@@ -1,34 +1,23 @@
-import configureMockStore from 'redux-mock-store'
-import createSagaMiddleware from 'redux-saga'
-import axios from 'axios'
-import { authUrl } from '../config/config'
-import { LOG_IN_REQUEST } from '../reducers/user'
+import { all, fork, put, takeLatest, call } from 'redux-saga/effects'
+import { LOG_IN_REQUEST, LOG_IN_SUCCESS } from '../reducers/user'
+import { testUserInfo } from '../config/config'
+import { logIn, logInAPI } from './user'
 
-const sagaMiddleware = createSagaMiddleware()
-const middlewares = [sagaMiddleware]
-
-axios.defaults.baseURL = authUrl
-describe("auth async request",()=>{
-    t('creates AUTH_USER action when user is logged in', () => {
-          axios.post("/auth/login")
-          .then( { data: 'Logged in successfully' }})
-    
-        const expectedActions = [
-          { type: LOG_IN_REQUEST }
-        ]
-        const store = mockStore({ })
-    
-        return store.dispatch(actions.loginUser({'no':"12345","password":'12345'}))
-          .then(() => { // return of async actions
-            expect(store.getActions()).toEqual(expectedActions)
-          })
+describe('auth request', () => {
+  it('사용자가 로그인을 하는 api를 호출하고 로그인 상태로 만들고, 정상 종료한다.', () => {
+    const iterator = logIn({
+      type: LOG_IN_REQUEST,
+      data: testUserInfo,
+    })
+    // 1. logIn Api 호출
+    expect(iterator.next().value).toEqual(call(logInAPI, testUserInfo))
+    // 2. 로그인 상태로 만들기
+    expect(iterator.next().value).toEqual(
+      put({
+        type: LOG_IN_SUCCESS,
       })
-
-function t(arg0: string, arg1: () => void) {
-  throw new Error('Function not implemented.')
-}
-
-
-function mockStore(arg0: {}) {
-  throw new Error('Function not implemented.')
-}
+    )
+    // 3. 실행 완료
+    expect(iterator.next().done).toBeTruthy()
+  })
+})
