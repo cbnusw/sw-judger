cb6dff3b1d03f45c46ef33e60fb066e990ae5e69
cbnusw <68525282+cbnusw@users.noreply.github.com>
2022-02-06 15:47:41 +0900

    Delete auth directory

--- auth/config/pm2/pm2.js
+++
@@ -1,6 +0,0 @@
-module.exports = (name, script, instance, exec_mode) => {
-  instance = +(instance || 0);
-  exec_mode = exec_mode || undefined;
-
-  return { apps: [{ name, script, instance, exec_mode }] };
-};
--- auth/utils/logger.js
+++
@@ -1,3 +0,0 @@
-const { AUTH_APP_LOG_DIR: LOG_DIR } = require('../shared/env');
-
-module.exports = require('../shared/utils/logger')(LOG_DIR);
