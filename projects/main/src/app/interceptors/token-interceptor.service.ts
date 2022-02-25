54d10c46e1c107cd8f6249ef4ff5d487956133ac
Jiwon Kim <lost731@naver.com>
2022-02-25 21:02:06 +0900

    Add: 메타태그 for 검색엔진 최적화

--- projects/main/src/app/interceptors/token-interceptor.service.ts
+++ projects/main/src/app/interceptors/token-interceptor.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { BehaviorSubject, from, Observable, throwError } from 'rxjs';
 import { catchError, filter, switchMap, take } from 'rxjs/operators';
@@ -12,36 +7,30 @@ import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, StorageService, TOKEN_FLUSH_EVENT 
 import { ERROR_CODES } from '../constants/error-codes';
 import { environment } from '../../environments/environment';
 
-
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-
   private isRefreshing = false;
   private refreshTokenSubject: BehaviorSubject<string> = new BehaviorSubject(null);
 
-  constructor(
-    private storageService: StorageService,
-    private router: Router
-  ) { }
+  constructor(private storageService: StorageService, private router: Router) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
     request = this.addToken(request);
 
     return next.handle(request).pipe(
-      catchError(err => {
+      catchError((err) => {
         if (err.error && err.error.code === ERROR_CODES.ACCESS_TOKEN_EXPIRED) {
           return this.refreshToken(request, next);
         }
         return throwError(err);
       }),
-      catchError(err => {
-        const { status } = (err || {}).error || (err || {});
-        if (status === 401 || status === 401) {
+      catchError((err) => {
+        const { status } = (err || {}).error || err || {};
+        if (status === 401) {
           const loginPageUrl = environment.loginPageUrl;
           this.storageService.clear();
           this.storageService.emit(TOKEN_FLUSH_EVENT);
-          loginPageUrl.startsWith('/') ? this.router.navigateByUrl(loginPageUrl) : location.href = loginPageUrl;
+          loginPageUrl.startsWith('/') ? this.router.navigateByUrl(loginPageUrl) : (location.href = loginPageUrl);
         }
         return throwError(err);
       })
@@ -64,11 +53,11 @@ export class TokenInterceptor implements HttpInterceptor {
         fetch(url, {
           method: 'GET',
           headers: {
-            'x-refresh-token': this.storageService.get(REFRESH_TOKEN_KEY)
-          }
-        }).then(res => res.json())
+            'x-refresh-token': this.storageService.get(REFRESH_TOKEN_KEY),
+          },
+        }).then((res) => res.json())
       ).pipe(
-        switchMap(res => {
+        switchMap((res) => {
           const { accessToken, refreshToken } = res.data;
           this.storageService.set(ACCESS_TOKEN_KEY, accessToken);
           this.storageService.set(REFRESH_TOKEN_KEY, refreshToken);
@@ -79,7 +68,7 @@ export class TokenInterceptor implements HttpInterceptor {
       );
     } else {
       return this.refreshTokenSubject.pipe(
-        filter(token => !!token),
+        filter((token) => !!token),
         take(1),
         switchMap(() => next.handle(this.addToken(request)))
       );
