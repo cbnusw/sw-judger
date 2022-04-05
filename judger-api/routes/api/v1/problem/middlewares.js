4eb9a4bf00697f664d8321f88fb863c8151e3661
cbnusw <cbnusw.oss@gmail.com>
2022-04-06 00:54:37 +0900

    Fix: 문제 엑세스 변경

--- judger-api/routes/api/v1/problem/middlewares.js
+++ judger-api/routes/api/v1/problem/middlewares.js
@@ -2,18 +2,32 @@ const asyncHanlder = require('express-async-handler');
 const { IS_NOT_TEST_PERIOD, LOGIN_REQUIRED, IS_NOT_CONTESTANT } = require('../../../../errors');
 const { hasRole } = require('../../../../utils/permission');
 const { Problem } = require('../../../../models');
-const { isAssigned, parentNotFoundErrors, checkOwnerOf } = require('./service.js');
+const { isAssigned,
+        parentNotFoundErrors, 
+        checkOwnerOf, 
+        checkTestPeriodOf, 
+        isPublished
+      } = require('./service.js');
 
 const handleAccessProblem = asyncHanlder(async (req, res, next) => {
   const { params: { id }, user } = req;
   const problem = await Problem.findById(id).populate('parentId');
-  // if (problem.published && isPublished(problem)) next();
   if (!user) return next(LOGIN_REQUIRED);
   const { parentId, parentType } = problem;
   if (parentType && !parentId) return next(parentNotFoundErrors[parentType]);
   if (checkOwnerOf(problem, user) || hasRole(user)) return next();
-  // if (!checkTestPeriodOf(parent)) return next(IS_NOT_TEST_PERIOD);
-  if ((parentType === "Assignment" || parentType === "Contest") && !await isAssigned(user, parentId, parentType)) return next(IS_NOT_CONTESTANT);
+  if (problem.published) {
+    if (!isPublished(problem)) {
+      if (!checkTestPeriodOf(parentId)) return next(IS_NOT_TEST_PERIOD);
+      if (
+        (parentType === "Assignment" || parentType === "Contest") && !(await isAssigned(user, parentId, parentType)))
+          return next(IS_NOT_CONTESTANT);
+      }
+  } else if (!problem.published)
+    if (!checkTestPeriodOf(parentId)) return next(IS_NOT_TEST_PERIOD);
+  if (
+    (parentType === "Assignment" || parentType === "Contest") &&!(await isAssigned(user, parentId, parentType)))
+      return next(IS_NOT_CONTESTANT);
   next();
 });
 
