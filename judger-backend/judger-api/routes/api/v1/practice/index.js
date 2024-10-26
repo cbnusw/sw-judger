de9c32d67ce20943b57f21537001fb0551fc0346
Minnnning <6alswjd6@gmail.com>
2024-10-26 15:54:31 +0900

    Feat: 연습문제 예제파일 api 추가

--- judger-backend/judger-api/routes/api/v1/practice/index.js
+++ judger-backend/judger-api/routes/api/v1/practice/index.js
@@ -8,6 +8,7 @@ router.get('/', controller.getPractices);
 router.get('/:id', isAuthenticated, controller.getPractice);
 router.get('/:id/my-submits', isAuthenticated, controller.getMyPracticeSubmits);
 router.get('/:id/submit/detail', isAuthenticated, controller.getSubmit);
+router.get('/:problemId/example-files/:fileId', controller.downloadExampleFile);
 router.post('/', isAuthenticated, controller.createPractice);
 router.post('/:id/submit', isAuthenticated, controller.createPracticeSubmit);
 router.patch('/:id', isAuthenticated, controller.updatePractice);
