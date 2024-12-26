591074b3c54124690179384d7be2eb3f16a47ac5
developersung13 <developersung13@gmail.com>
2024-12-26 15:26:49 +0900

    feat: 마이페이지 내 RWD 적용

--- judger-frontend/app/mypage/profile/page.tsx
+++ judger-frontend/app/mypage/profile/page.tsx
@@ -18,46 +18,50 @@ export default function Profile() {
       <div className="border-t-[3px] mt-4 border-black" />
       <div className="flex flex-col mt-4 tracking-tight text-xs">
         <div className="flex items-center pb-3 border-b ">
-          <span className="w-44 text-sm">학번</span>
-          <span className="w-80 font-bold">{userInfo.no}</span>
+          <span className="w-[6rem] 3md:w-[11rem] text-sm">학번</span>
+          <span className="w-fit font-bold">{userInfo.no}</span>
         </div>
-        <div className="flex items-center py-3 border-b">
-          <div className="w-44 text-sm">비밀번호</div>
-          <div className="w-80 font-bold">********</div>
+        <div className="flex flex-col 3md:flex-row items-start 3md:items-center gap-3 3md:gap-0 py-3 border-b">
+          <div className="flex items-center">
+            <div className="w-[6rem] 3md:w-[11rem] text-sm">비밀번호</div>
+            <div className="w-fit 2lg:w-80 font-bold">********</div>
+          </div>
           <a
             href="https://sw7up.cbnu.ac.kr/my-page/password"
             target="_blank"
-            className="flex justify-center items-center gap-[0.375rem] text-[0.8rem] text-[#4e5968] bg-[#f2f4f6] px-4 py-[0.5rem] rounded-[7px] font-medium  hover:bg-[#d3d6da]"
+            className="w-full ml-auto 2lg:ml-0 3md:w-fit flex justify-center items-center gap-[0.375rem] text-[0.8rem] text-[#4e5968] bg-[#f2f4f6] px-4 py-[0.5rem] rounded-[7px] font-medium  hover:bg-[#d3d6da]"
           >
             비밀번호 변경
           </a>
         </div>
-        <div className="flex items-center py-3 border-b">
-          <div className="w-44 text-sm">이름(실명)</div>
-          <div className="w-80 font-bold">{userInfo.name}</div>
+        <div className="flex flex-col 3md:flex-row items-start 3md:items-center gap-3 3md:gap-0 py-3 border-b">
+          <div className="flex items-center">
+            <div className="w-[6rem] 3md:w-[11rem] text-sm">이름(실명)</div>
+            <div className="w-fit 2lg:w-80 font-bold">{userInfo.name}</div>
+          </div>
           <a
             href="https://sw7up.cbnu.ac.kr/my-page/info"
             target="_blank"
-            className="flex justify-center items-center gap-[0.375rem] text-[0.8rem] text-[#4e5968] bg-[#f2f4f6] px-4 py-[0.5rem] rounded-[7px] font-medium  hover:bg-[#d3d6da]"
+            className="w-full ml-auto 2lg:ml-0 3md:w-fit flex justify-center items-center gap-[0.375rem] text-[0.8rem] text-[#4e5968] bg-[#f2f4f6] px-4 py-[0.5rem] rounded-[7px] font-medium  hover:bg-[#d3d6da]"
           >
             개인정보 변경
           </a>
         </div>
         <div className="flex items-center py-3 border-b">
-          <div className="w-44 text-sm">이메일</div>
-          <div className="w-80 font-bold">{userInfo.email}</div>
+          <div className="w-[6rem] 3md:w-[11rem] text-sm">이메일</div>
+          <div className="w-fit font-bold">{userInfo.email}</div>
         </div>
         <div className="flex items-center py-3 border-b">
-          <div className="w-44 text-sm">대학</div>
-          <div className="w-80 font-bold">{userInfo.university}</div>
+          <div className="w-[6rem] 3md:w-[11rem] text-sm">대학</div>
+          <div className="w-fit font-bold">{userInfo.university}</div>
         </div>
         <div className="flex items-center py-3 border-b">
-          <div className="w-44 text-sm">학부(과)</div>
-          <div className="w-80 font-bold">{userInfo.department}</div>
+          <div className="w-[6rem] 3md:w-[11rem] text-sm">학부(과)</div>
+          <div className="w-fit font-bold">{userInfo.department}</div>
         </div>
         <div className="flex items-center py-3 border-b">
-          <div className="w-44 text-sm">휴대전화</div>
-          <div className="w-80 font-bold">{userInfo.phone}</div>
+          <div className="w-[6rem] 3md:w-[11rem] text-sm">휴대전화</div>
+          <div className="w-fit font-bold">{userInfo.phone}</div>
         </div>
       </div>
     </div>
