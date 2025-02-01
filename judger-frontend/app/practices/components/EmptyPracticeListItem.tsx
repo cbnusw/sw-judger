8e170838768c9f4196bd41166bf5e7b9c3d0f9ec
developersung13 <developersung13@gmail.com>
2025-02-01 19:39:50 +0900

    chore: 연습문제 목록 리스트 아이템 미 존재에 대한 렌더링 컴포넌트 UI 수정

--- judger-frontend/app/practices/components/EmptyPracticeListItem.tsx
+++ judger-frontend/app/practices/components/EmptyPracticeListItem.tsx
@@ -1,15 +1,6 @@
+import NotFound from '@/app/components/NotFound';
 import React from 'react';
 
 export default function EmptyPracticeListItem() {
-  return (
-    <tr className="h-[2.5rem] border-b-[1.25px] border-[#d1d6db] text-xs text-center">
-      <th
-        scope="row"
-        className="px-2 py-2 font-normal text-[#4e5968] whitespace-nowrap dark:text-white"
-      >
-        1
-      </th>
-      <td className="text-[#4e5968]">등록된 연습문제 정보가 없어요</td>
-    </tr>
-  );
+  return <NotFound message={'조회 가능한 문제 정보가 없어요'} />;
 }
